92942a4a2f98b40224c51de73fd1b155f9843241
ee <wewfwfw>
2025-02-15 09:30:11 +0200

    --

---
+++ hw10.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+connection = sqlite3.connect('hw10.sl3', timeout=5)
+cursor = connection.cursor()
+
+cursor.execute("create table users (City text, temperature text, time text)")
+
+current_time = '14.11.2025'
+
+cursor.execute("insert into users (City, temperature, time) values ('Київ', '9°C', ?)", (current_time,))
+
+cursor.execute("select * from users")
+print(cursor.fetchall())
+
+connection.commit()
